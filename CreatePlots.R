586a3156bd4eb008c953b7e0af35153cba248b29
R. Blake Hill Lab (MCW) <34748371+Hill-Lab@users.noreply.github.com>
2018-02-06 11:10:21 -0600

    Update CreatePlots.R

--- CreatePlots.R
+++ CreatePlots.R
@@ -1,6 +1,11 @@
+
+# First add a column, in the second column position, to "output-summary.csv" labeled with "Condition" and filled in with values for conditions tested
 #
-# Run with Command + Option + R
+# Set working directory to source file location (session -> set working directory -> to source file location)
+# 
+# Replace GnetsFolder with path where data is stored
 #
+# Run with Command+Option+r
 
 rm(list=ls())
 
@@ -11,13 +16,10 @@ library(RColorBrewer)
 
 transparent <- rgb(0, 0, 0, 0)
 
-GnetsFolder <- readline(prompt="Folder containing .gnet files: ")
+# Replace with path specific for your data, make sure to include final "/" after the name
+GnetsFolder <- "~/Dropbox/Analytical_Biochemistry_Method/R_scripts_sample_data/" 
 
-Summary <- read.csv(paste(GnetsFolder,"/output-summary.csv",sep=""))
-
-if (length(which(names(Summary)=='Condition'))==0) {
-  stop("The column named 'Condition' seems to be missing in output-summary.csv. Aborting...")
-}
+Summary <- read.csv(paste(GnetsFolder,"output-summary.csv",sep=""))
 
 # Conditions will appear in the same order as in the CSV summary file
 Summary$Condition <- as.character(Summary$Condition)
@@ -25,10 +27,9 @@ Summary$Condition <- factor(Summary$Condition, levels=unique(Summary$Condition))
 
 avgSummary <- aggregate(data=Summary,.~Condition,FUN=mean)
 stdSummary <- aggregate(data=Summary,.~Condition,FUN=sd)
-ids <- which(names(stdSummary) %in% c('Condition','File_Name'))
-names(stdSummary)[-ids] <- paste(names(stdSummary)[-ids],'_sd',sep='')
+names(stdSummary)[-c(1,2)] <- paste(names(stdSummary)[-c(1,2)],'_sd',sep='')
 
-Summary_AVG_STD <- cbind(avgSummary[,-ids[2]],stdSummary[,-ids])
+Summary_AVG_STD <- cbind(avgSummary[,-2],stdSummary[,-c(1,2)])
 
 # Expands the number of color palette options to fit the number of conditions in the dataset.
 # To see all available choices simply attach the package with library(RColorBrewer) 
@@ -76,7 +77,7 @@ for (p in seq(1,length(PlotsToBeMade),2)) {
                    8,0.005)
 
   
-  fig <- plot(ggplot(data=Summary_AVG_STD,aes_string(x=feature1, y=feature2, color="Condition")) + geom_point(size=5) +
+  plot(ggplot(data=Summary_AVG_STD,aes_string(x=feature1, y=feature2, color="Condition")) + geom_point(size=5) +
     geom_errorbar(aes_string(ymin=dy_min, ymax=dy_max), width=error[p], show.legend=FALSE) +
     geom_errorbarh(aes_string(xmin=dx_min, xmax=dx_max), height=error[p+1], show.legend=FALSE) +
     #geom_point(data=Summary,aes_string(x=feature1, y=feature2, color="Condition")) + geom_point(size=2) + # comment out this line to remove individual points
@@ -102,8 +103,7 @@ for (p in seq(1,length(PlotsToBeMade),2)) {
       # ylim(AxisMinimum[p+1], AxisMaximum[p+1])
       )
   
-    #print(fig)
-    ggsave(paste(GnetsFolder,"/Plot-",p,".eps",sep=""), width = 14, height = 16, units = "cm", dpi = 300)
+    ggsave(paste(GnetsFolder,"Plot-",p,".eps",sep=""), width = 14, height = 16, units = "cm", dpi = 300)
 
 }
 
@@ -113,8 +113,8 @@ for (p in seq(1,length(PlotsToBeMade),2)) {
 
 VarNames <- c("PHI", "Avg_Edge_Length_um", "Total_Edge_Norm_to_Length_um", "Total_Node_Norm_to_Length_um", "Total_Connected_Components_Norm_to_Length_um", "Free_Ends", "three_way_junction", "four_way_junction", "Avg_Degree")
 
-avgSummary_long <- melt(avgSummary[,-ids[2]], id.vars=c("Condition"))
-stdSummary_long <- melt(stdSummary[,-ids[2]], id.vars=c("Condition"))
+avgSummary_long <- melt(avgSummary[,-2], id.vars=c("Condition"))
+stdSummary_long <- melt(stdSummary[,-2], id.vars=c("Condition"))
 
 Summary_AVG_STD_long <- cbind(avgSummary_long,data.frame("value_sd"=stdSummary_long$value))
 
@@ -122,7 +122,7 @@ Vars <- which(!is.na(match(Summary_AVG_STD_long$variable,VarNames)))
 
 Summary_AVG_STD_long <- Summary_AVG_STD_long[Vars,]
 
-fig <- ggplot(Summary_AVG_STD_long, aes(fill = Condition, x = variable, y = value)) +
+ggplot(Summary_AVG_STD_long, aes(fill = Condition, x = variable, y = value)) +
   geom_col(position = position_dodge (width = 0.9)) +
   scale_fill_manual(values = getPalette(nc)) +
   geom_errorbar(aes(ymin=value-value_sd, ymax=value+value_sd), position=position_dodge(width = 0.9), width=0.5, color="grey30", size=0.5, show.legend=FALSE) +
@@ -150,20 +150,19 @@ fig <- ggplot(Summary_AVG_STD_long, aes(fill = Condition, x = variable, y = valu
         legend.text = element_text(size = 12, color = "black")
   )
 
-print(fig)
-ggsave(paste(GnetsFolder,"/AVG_SD_metrics.eps",sep=""), width = 18, height = 16, units = "cm", dpi = 300)
+ggsave(paste(GnetsFolder,"AVG_SD_metrics.eps",sep=""), width = 18, height = 16, units = "cm", dpi = 300)
 
 #
 # ALL METRICS BOX and Scatter PLOTS
 #
 
-Summary_long <- melt(Summary[,-ids[2]], id.vars=c("Condition"))
+Summary_long <- melt(Summary[,-1], id.vars=c("Condition"))
 
 Vars <- which(!is.na(match(Summary_long$variable,VarNames)))
 
 Summary_long <- Summary_long[Vars,]
 
-fig <- ggplot(Summary_long, aes(fill=Condition, x=variable, y=value)) +
+ggplot(Summary_long, aes(fill=Condition, x=variable, y=value)) +
   stat_boxplot(geom = "errorbar", colour = "grey15", width = 0.5, position = position_dodge (width = 1)) +
   geom_boxplot (outlier.size = 0, colour = "grey15", position = position_dodge (width = 1)) +
   scale_fill_manual(values = getPalette(nc)) +
@@ -195,10 +194,9 @@ fig <- ggplot(Summary_long, aes(fill=Condition, x=variable, y=value)) +
         legend.text = element_text(size = 8, color = "black")
   )
 
-print(fig)
-ggsave(paste(GnetsFolder,"/All_metrics.eps",sep=""), width = 20, height = 16, units = "cm", dpi = 300)
+ggsave(paste(GnetsFolder,"All_metrics.eps",sep=""), width = 20, height = 16, units = "cm", dpi = 300)
 
-fig <- ggplot(Summary, aes(fill = Condition, x = Condition, y = MitoGraph_Connectivity_Score)) +
+ggplot(Summary, aes(fill = Condition, x = Condition, y = MitoGraph_Connectivity_Score)) +
   stat_boxplot(geom = "errorbar", width = 0.5, colour = "grey15") +  
   geom_boxplot(colour = "grey15") +
   scale_fill_manual(values = getPalette(nc)) +
@@ -222,26 +220,61 @@ fig <- ggplot(Summary, aes(fill = Condition, x = Condition, y = MitoGraph_Connec
         legend.text = element_text(size = 12, color = "black")
   )
 
-print(fig)
-ggsave(paste(GnetsFolder,"/MitoGraph_Connectivity_score.eps",sep=""), width = 20, height = 16, units = "cm",
+ggsave(paste(GnetsFolder,"MitoGraph_Connectivity_score.eps",sep=""), width = 20, height = 16, units = "cm",
        dpi = 300)
 
-write.table(Summary_AVG_STD_long, file = paste(GnetsFolder,"/Summary_AVG_STD.csv",sep=""), sep = ",", quote = FALSE, row.names = F)
+write.table(Summary_AVG_STD_long, file = paste(GnetsFolder,"Summary_AVG_STD.csv",sep=""), sep = ",", quote = FALSE, row.names = F)
 formattable(Summary_AVG_STD_long, row.names = F, list( " " = FALSE))
 
-# AOV analysis if more than one condition is found
+# ########################
+# Alternative code for AOV
+# #########################
 
-if (length(levels(Summary$Condition)) > 1) {
-
-  AOV_Stats_New <- NULL
-  for (var in seq(3,21,1)) {
-    Temp <- data.frame('Condition'=Summary$Condition,'Var'=Summary[,var])
-    Test <- TukeyHSD(aov(data=Temp, Var~Condition))
-    AOV_Stats_New <- rbind(AOV_Stats_New,data.frame('Comparison'=row.names(Test$Condition),'Variable'=names(Summary)[var],'p.value'=Test$Condition[4]))
-  }
-  
-  head(AOV_Stats_New)
-  write.csv(AOV_Stats_New, paste(GnetsFolder,"/AOV_stats_new.csv",sep=""))
-  ftable <- formattable(AOV_Stats_New, p.value=formatter("span", style = x~style(color=ifelse(x < 0.05 , "green", "black"))))
-  print(ftable)
+AOV_Stats_New <- NULL
+for (var in seq(3,21,1)) {
+  Temp <- data.frame('Condition'=Summary$Condition,'Var'=Summary[,var])
+  Test <- TukeyHSD(aov(data=Temp, Var~Condition))
+  AOV_Stats_New <- rbind(AOV_Stats_New,data.frame('Comparison'=row.names(Test$Condition),'Variable'=names(Summary)[var],'p.value'=Test$Condition[4]))
 }
+
+head(AOV_Stats_New)
+write.csv(AOV_Stats_New, paste(GnetsFolder,"AOV_stats_new.csv",sep=""))
+formattable(AOV_Stats_New, p.value=formatter("span", style = x~style(color=ifelse(x < 0.05 , "green", "black"))))
+#
+# Not shown in color (fix it)
+#
+
+
+# ###############################
+# End of alternative code for AOV
+# ###############################
+
+# # John's aov() approach using tidyverse's dplyr and tidyr and broom package ----
+# library(tidyverse)
+# library(broom)
+# library(dplyr)
+# 
+# AOV_stats <- Summary %>% # perform ANOVA & post-hoc Tukey test for all groups
+#   gather(., "var", "value", 3:21) %>%
+#   group_by(., var) %>%
+#   do(tidy(TukeyHSD(aov(value ~ Condition, data = .))))
+# 
+# write_csv(AOV_stats, "AOV_stats.csv")
+# formattable(AOV_stats, list(term = FALSE, estimate = FALSE, conf.low = FALSE, conf.high = FALSE, 
+#                             adj.p.value = formatter("span", style = x ~ style(color = ifelse(x < 0.05 , "green", "red")))))
+
+# uncomment to gain information on residuals
+# AOV_residuals <- Summary %>% # obtain residuals and fitted values
+#   gather(., "var", "value", 3:21) %>% 
+#   group_by(., var) %>%
+#   do(augment(aov(value ~ Condition, data = .)))
+# write_csv(AOV_residuals, "AOV_residuals.csv")
+
+# uncomment to gain information on residuals
+# AOV_model <- Summary %>% # obtain information on model (R^2, adj. R^2, etc.)
+#   gather(., "var", "value", 3:21) %>% # it may be that we don't need this info
+#   group_by(., var) %>%
+#   do(glance(aov(value ~ Condition, data = .)))
+# write_csv(AOV_model, "AOV_model.csv")
+
+# end of John's ANOVA code ----
